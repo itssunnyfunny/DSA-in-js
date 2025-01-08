990cf47ea413ec93ffb263935b723b911a1d3545
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-08 14:42:39 +0530

    basic array methods

---
+++ array/index.js
@@ -0,0 +1,54 @@
+// do lot of array and str functions 
+const arr = [1,2,45,5,34,5,67,7,6];
+
+//1 length
+//console.log(arr.length);
+
+//2 toString = return a string
+
+ const str = arr.toString()
+
+
+ //3 at - what is the value at index
+
+ console.log(arr.at(2));
+
+ //4 join - return a string
+
+  const jin = arr.join('@')
+
+
+  //5 pop() - remove the last elm of the attach array
+
+  arr.pop() 
+
+
+  //6 push() - adding a elm to a array from last
+  arr.push(100)
+
+
+//7 shift - remove the first elm of array
+
+arr.shift()
+
+
+//8 unshift() - add a new elm at the starting of array
+
+arr.unshift(1)
+
+
+
+//9 
+
+
+  
+  
+
+
+  
+ 
+
+
+ 
+
+ 
