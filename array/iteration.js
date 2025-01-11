d560bfb03404dbc7d0eee3f9c242d795b501e423
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-11 15:13:13 +0530

    i know the for each is only iterate on the given array

--- array/iteration.js
+++ array/iteration.js
@@ -1,1 +1,12 @@
 // iterating on the given array
+const arr = [1,2,3,4,6,5]
+
+//1 for each - it is for itarating on the array don't return any thing i guess
+let text = ""
+arr.forEach(myfunction)
+ function myfunction(value) {
+    text += value + "<br>"
+ }
+// console.log(text);
+
+
