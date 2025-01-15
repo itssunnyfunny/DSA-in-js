af91476d410c606addbe56f57440f61387cbc691
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-15 11:46:04 +0530

    searching the index of giving elm

--- array/search.js
+++ array/search.js
@@ -1,1 +1,14 @@
 // this file is for seaching methods on a given array
+
+// 1 indexof() - finding the index of a given elem
+
+const fruits = ["apple" , "orange", "banana","orange" , "mango"];
+
+let Position = fruits.indexOf("banana");
+// console.log(Position);
+
+// 2 lastIndexOf() - find the index but search from the end (last elm);
+
+let secondPst = fruits.lastIndexOf("orange")
+// console.log(secondPst);
+
