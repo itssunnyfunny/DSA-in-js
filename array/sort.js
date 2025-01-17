5f18c57c3d09b7caaff5f721d1b2a2a6276fac9a
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-17 13:40:03 +0530

    sorting the array

--- array/sort.js
+++ array/sort.js
@@ -1,1 +1,11 @@
 // its for sorting the arrays
+
+
+// Alphabetical sorting
+
+// 1 sort() - sorting the array alphabetically
+const fruits = ["banana", "orange", "apple", "mango"];
+const sorted = fruits.sort()
+// console.log(sorted);
+
+// 2 reverse() - 
