c21960dd7743357a42802e4b183e55d7eb1547c9
itssunnyfunny <sunnyfunny4062@gmail.com>
2025-01-21 14:32:38 +0530

    easy q sum and max of arr

---
+++ array/problems/easy.js
@@ -0,0 +1,17 @@
+// 1 sum of given array
+const arr = [1,2,3,4,5]
+
+function sumOfAll(arr) {
+  return  arr.reduce((p, a)=> a+p)
+}
+
+
+//2 max value in a array
+
+function maxVal(arr) {
+    return Math.max.apply(null, arr)
+}
+
+
+// 
+
